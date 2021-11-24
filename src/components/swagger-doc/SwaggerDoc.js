f290231e1fa79a9b381a84ad7907f713326d7cc1
shoom3301 <shoom3301@gmail.com>
2021-11-24 13:41:38 +0600

    Copy swagger styles

--- src/components/swagger-doc/SwaggerDoc.js
+++ src/components/swagger-doc/SwaggerDoc.js
@@ -2,6 +2,6 @@ import {Buffer} from 'buffer';
 global['Buffer'] = Buffer;
 
 import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+import './swagger-ui.css';
 
 export {SwaggerUI as SwaggerDoc};
