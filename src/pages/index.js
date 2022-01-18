ccc80e07ee5412776196ff6a394519118a3c4e13
Maria Kurmacheva <tscherkasova.mascha@gmail.com>
2022-01-18 12:20:08 +0500

    change cover

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import HomepageFooter from '../components/homepage-footer/HomepageFooter';
 import HomePageBackgroundImage from '../../static/img/logo-background.svg'
 import Layout from "@docusaurus/core/lib/client/theme-fallback/Layout";
 import Head from "@docusaurus/core/lib/client/exports/Head";
-import Cover from '../../static/img/cover.jpg';
+import Cover from '../../static/img/cover.png';
 
 function HomepageHeader() {
     return (
@@ -21,7 +21,7 @@ export default function Home() {
         <Layout pageClassName={styles.mainLayout}
                 description="The 1inch Network unites decentralized protocols whose synergy enables the most lucrative, fastest and protected operations in the DeFi space.">
             <Head>
-                <meta name="og:image" content={Cover} />
+                <meta property="og:image" content={Cover} />
                 <meta name="twitter:image" content={Cover} />
                 <title>1inch Network | Leading high capital efficient DeFi protocols</title>
             </Head>
