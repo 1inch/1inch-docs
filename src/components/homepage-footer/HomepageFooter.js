d4aae183e8a4d0b9fdc7ef5bdd0d0833ef0c84a4
Kurmachev Pavel <pavelkurmachev@gmail.com>
2024-03-14 15:26:56 +0500

    rename Terms of Service to Terms of use

--- src/components/homepage-footer/HomepageFooter.js
+++ src/components/homepage-footer/HomepageFooter.js
@@ -65,7 +65,7 @@ const FooterLinks = {
                     link: 'https://1inch.io/press-room/'
                 },
                 {
-                    title: 'Terms of service',
+                    title: 'Terms of use',
                     link: 'https://1inch.io/assets/1inch_network_terms_of_use.pdf'
                 },
                 {
