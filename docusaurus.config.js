03c90abec4a2c45c1021cfd9ceeb8b3034397bc6
tor-por <ekaterina9836@yandex.ru>
2024-04-23 19:18:41 +0300

    Navbar fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,11 +81,6 @@ const config = {
             ],
           },
           {
-            label: 'Fusion',
-            position: 'left',
-            href: '/docs/fusion-swap/introduction/',
-          },
-          {
             label: 'Governance',
             position: 'left',
             type: 'doc',
@@ -114,14 +109,18 @@ const config = {
             ],
           },
           {
-            label: 'Grants Program',
-            position: 'right',
-            href: 'https://1inch.io/foundation-grant-program/',
-          },
-          {
-            label: 'Bug bounty',
-            position: 'right',
-            href: 'https://airtable.com/shrddQOOkm3hOLuzd',
+            "label": "Developer links",
+            "position": "right",
+            "items": [
+              {
+                label: 'Grants Program',
+                href: 'https://1inch.io/foundation-grant-program/',
+              },
+              {
+                label: 'Bug bounty',
+                href: 'https://airtable.com/shrddQOOkm3hOLuzd',
+              }
+            ]
           },
           {
             href: 'https://www.npmjs.com/search?q=%401inch',
